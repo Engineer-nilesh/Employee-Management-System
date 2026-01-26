94077e5e391e37b99381aa64951423ba19495059
Nilesh <Admin@ROYAL-NILESH>
2026-01-26 22:53:51 +0530

    Add EF Core SQL Server support and initial migration
    - Register AppDbContext in DI and configure SQL Server connection
    - Add connection string to appsettings.json
    - Add AppDbContext with DbSets for core entities
    - Add initial EF Core migration and model snapshot
    - Update package versions and set ImplicitUsings to false
    - Reference BaseLibrary in ServerLibrary project

--- Employee Management System/Server/Program.cs
+++ Employee Management System/Server/Program.cs
@@ -1,3 +1,11 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ServerLibrary.Data;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +14,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+// Starting
+builder.Services.AddDbContext<AppDbContext>(options => 
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Sorry, Connection string not found!!"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
